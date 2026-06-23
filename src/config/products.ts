869a8741e1143df94d9fddf421a8f0bdef7a6db9
Corbin March <marchcorbin@gmail.com>
2026-06-22 23:20:54 -0600

    Bump download version to v2.0.165

--- src/config/products.ts
+++ src/config/products.ts
@@ -26,8 +26,8 @@ export const DOWNLOAD = {
 	installerLatest:
 		'https://ultiplay-updates.s3.us-east-1.amazonaws.com/releases/installer/Ultiplay_Setup_latest.exe',
 	installerVersioned:
-		'https://ultiplay-updates.s3.us-east-1.amazonaws.com/releases/installer/Ultiplay_Setup_2.0.164.exe',
-	currentVersion: '2.0.164',
+		'https://ultiplay-updates.s3.us-east-1.amazonaws.com/releases/installer/Ultiplay_Setup_2.0.165.exe',
+	currentVersion: '2.0.165',
 } as const;
 
 // ---------------------------------------------------------------------------
